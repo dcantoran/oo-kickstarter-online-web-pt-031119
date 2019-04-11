c5827f11309b3985eea6022338d77e3eb8666ec6
David Cantoran <davidcantoran36@gmail.com>
2019-04-11 23:52:31 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,13 @@
 class Project 
-  attr_reader :title, :backers 
+  attr_reader :title
   def initialize(title)
     @title = title 
     @@backers = []
   end 
   
+  def self.backers
+    @@backers
+  end 
   def add_backer(backer)
     @@backers << backer
     
