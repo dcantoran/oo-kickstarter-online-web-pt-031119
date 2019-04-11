32fd24063e45a9cdf9d34ee43f3b8957881cb1d3
David Cantoran <davidcantoran36@gmail.com>
2019-04-11 23:50:37 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,11 @@ class Backer
   attr_reader :name, :backed_projects
   def initialize(name)
     @name = name 
-    @backed_projects = []
+    @@backed_projects = []
   end 
   
   def back_project(prjct)
-    @backed_projects << prjct
+    @@backed_projects << prjct
     # Project.backers << prjct
   end 
   
